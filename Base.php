40ef61e1a9c47811ef213e867cc9f9282996e288
AiluroPanda31 <baron.baptiste@laposte.net>
2018-03-16 08:52:53 +0000

    Page php de base

---
+++ Base.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Base</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+</head>
+<body>
+    <div id="zoneAffichage" style="margin: auto;text-align: center;width: 500px;height: 500px;border: dotted">Zone du chat </div>
+    <button id="target">Button</button>
+    <input id="nom" type="text">
+    <input id="message" type="text">
+    <span style="display:none;"></span>
+    <script src="ScriptJS.js"></script>
+</body>
+</html>
