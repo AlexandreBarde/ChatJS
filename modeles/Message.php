6b4aea03f87940be40f28dcf01ae2664857c2e9f
Unknown <baron.baptiste@laposte.net>
2018-03-22 09:07:58 +0100

    AJout messageDAO + setters Message

--- modeles/Message.php
+++ modeles/Message.php
@@ -8,20 +8,44 @@ class Message {
    private $_user;
    private $_idMessage;
    
-   public function getUser(){
+   public function getUser()
+   {
       return $this->$_user->getPseudo();
    }
 
-   public function getContenu(){
+   public function getContenu()
+   {
       return $this->_contenu;
    }
    
-   public function getTimestamp(){
+   public function getTimestamp()
+   {
       return $this->_timestamp;
    }
    
-   public function getIdMessage(){
+   public function getIdMessage()
+   {
       return $this->_idMessage;
+   }
+   
+   public function setUser(User $user)
+   {
+      $this->_user = $user; 
+   }
+   
+   public function setContenu($contenu)
+   {
+      $this->_contenu = $contenu; 
+   }
+   
+   public function setTimestamp($timestamp)
+   {
+      $this->_timestamp = $timestamp; 
+   }
+   
+   public function setIdMessage($idM)
+   {
+      $this->_idMessage = $idM; 
    }
 }
  ?>
