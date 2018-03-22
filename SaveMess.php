778179e4061978def92694d3666c99bb768186a5
Unknown <baron.baptiste@laposte.net>
2018-03-22 08:45:05 +0100

    save mess

--- SaveMess.php
+++ SaveMess.php
@@ -1,6 +1,6 @@
 <?php
 $linkpdo = new PDO('mysql:host=localhost;dbname=brb2528a','brb2528a','A4dkhmyB');
 $ts = time();
-$rq = 'INSERT INTO message(TimeStamp,ID_Gens,Message) VALUES("'.$ts.'",(SELECT ID_Gens FROM Gens WHERE nom ="'.$_GET["auteur"].'"),"'.$_GET["message"].'")';
+$rq = 'INSERT INTO message(timeStamp,id_compte,contenu) VALUES("'.$ts.'",(SELECT id_compte FROM compte WHERE pseudo ="'.$_GET["auteur"].'"),"'.$_GET["message"].'")';
 $res = $linkpdo -> prepare($rq);
 $res -> execute();
