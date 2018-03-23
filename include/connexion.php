4d48181030eca2e4caf7fe4f28d046d82b329e2a
Alexandre <barde.alexandre@yahoo.fr>
2018-03-23 10:18:28 +0100

    Un peu de css

--- include/connexion.php
+++ include/connexion.php
@@ -5,8 +5,12 @@
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link rel="stylesheet" href="include/style.css">
     </head>
     <body>
+        <div class="jumbotron jumbotron-fluid jumbotron-header">
+            <h1 class="display-3 text-center jumbotron-title">Authentification</h1>
+        </div>
         <h2 class="text-center display-4">Veuillez vous connecter pour accéder à l'application</h2>
         <div class="container" id="auth">
             <div class="row">
