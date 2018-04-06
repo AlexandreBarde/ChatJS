8f5f7cff76c13a74b3329750160c9467fae8af80
Unknown <baron.baptiste@laposte.net>
2018-04-06 23:15:13 +0200

    Mise en page

--- chat/index.php
+++ chat/index.php
@@ -2,8 +2,7 @@
 
 session_start();
 
-if(!isset($_SESSION['username']))
-{
+if (!isset($_SESSION['username'])) {
     $_SESSION['erreur'] = 2;
     header("Location: ../");
 }
@@ -17,47 +16,63 @@ if(!isset($_SESSION['username']))
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous"><title>ChatJS</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css"
+          integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css"
+          integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous">
+    <title>ChatJS</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 </head>
-    <body>
-    <nav class="navbar navbar-light bg-light justify-content-between">
-        <a class="navbar-brand">ChatJS</a>
-        <span id="username" class="navbar-text"><?= $_SESSION['username'] ?></span>
-        <a href="destroy.php"><button type="button" class="btn btn-outline-danger">Se déconnecter</button></a>
-    </nav>
-    <br />
-    <div class="container">
-        <div class="card-deck">
-            <div class="card">
-                <h5 class="card-header">Derniers messages du chat</h5>
-                <div id="affichage" class="card-body" style="height : 500px; overflow-y:scroll">
-                    <div id="affichageFin"></div>
-                </div>
-                <div id="affichage" class="card-body">
-                    <form class="form-row">
-                        <div class="col-10">
-                            <label class="sr-only" for="inlineFormInputName2">Message</label>
-                            <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Votre message">
+<body>
+<nav class="navbar navbar-light bg-light justify-content-between">
+    <a class="navbar-brand">ChatJS</a>
+    <span id="username" class="navbar-text"><?= $_SESSION['username'] ?></span>
+    <a href="destroy.php">
+        <button type="button" class="btn btn-outline-danger">Se déconnecter</button>
+    </a>
+</nav>
+<br/>
+<div class="container">
+    <div class="card-deck">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-9">
+                    <div class="card">
+                        <h5 class="card-header">Derniers messages du chat</h5>
+                        <div id="affichage" class="card-body" style="height : 500px; overflow-y:scroll">
+                            <div id="affichageFin"></div>
                         </div>
-                        <div class="col">
-                            <button id="boutonSubmit" type="submit" class="btn btn-primary mb-2">Envoyer</button>
-                            <span style="color: red" id="erreur"> </span>
+                        <div id="affichage" class="card-body">
+                            <form class="form-row">
+                                <div class="col-10">
+                                    <label class="sr-only" for="inlineFormInputName2">Message</label>
+                                    <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2"
+                                           placeholder="Votre message">
+                                </div>
+                                <div class="col">
+                                    <button id="boutonSubmit" type="submit" class="btn btn-primary mb-2">Envoyer
+                                    </button>
+                                    <span style="color: red" id="erreur"> </span>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
-            <div class="card">
-                <h5 class="card-header">Utilisateurs (<span id="nombreUtilisateurs"></span>)</h5>
-                <div class="card-body">
-                    <p id="utilisateurs" class="card-text">
-                    </p>
+                <div class="col-xl-3">
+                    <div class="card">
+                        <h5 class="card-header">Utilisateurs (<span id="nombreUtilisateurs"></span>)</h5>
+                        <div class="card-body">
+                            <p id="utilisateurs" class="card-text">
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <script src="ScriptJS.js"></script>
-    </body>
+</div>
+<script src="ScriptJS.js"></script>
+</body>
 </html>
